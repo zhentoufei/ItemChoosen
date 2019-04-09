1f4e4154b47cfa9ab610869009936a7b5cc4308d
xuling <xuling@meilin-inc.com>
2019-04-10 01:04:00 +0800

    bug fix

--- ComputeIncrease.py
+++ ComputeIncrease.py
@@ -13,26 +13,37 @@ def computeIncrease(start, end):
             new_file_path.append(src_path)
 
     for file_path in new_file_path:
+        print(file_path)
         cur_save_path = os.path.join(INCREASE_PATH, file_path.split('/')[-1].replace('msearchitem', 'msearchitem_increase'))
         src_df = pd.read_csv(file_path)
 
         if os.path.exists(cur_save_path):
-            df = pd.read_csv(cur_save_path)
-            df = pd.merge(src_df, df, how="left", on="itemid")
+            df = pd.read_csv(cur_save_path).unique()
+            del df['itemlink']
+            df = pd.merge(src_df[['itemid', 'itemlink']], df, how="left", on="itemid")
         else:
-            df = src_df[['itemid', 'itemlink']]
+            df = src_df[['itemid', 'itemlink']].unique()
 
-        start_col = [col for col in src_df.columns if col.find(start) >= 0][0]
-        end_col = [col for col in src_df.columns if col.find(end) >= 0][0]
+        start_col_list = [col for col in src_df.columns if col.find(start) >= 0]
+        end_col_list = [col for col in src_df.columns if col.find(end) >= 0]
+        if len(start_col_list) <= 0 or len(end_col_list) <= 0:
+            continue
+
+        start_col = start_col_list[0]
+        end_col = end_col_list[0]
+
 
         if end_col.replace('order_num', 'up') in df.columns:
             del df[end_col.replace('order_num', 'up')]
 
+
         df[end_col.replace('order_num', 'up')] = src_df[end_col]-src_df[start_col]
+
         df.sort_values(end_col.replace('order_num', 'up'), inplace=True, ascending=False)
+
         df.fillna(0, inplace=True)
         df.to_csv(cur_save_path, index=False)
-        print(df.head(10))
+        print(df.columns)
 
 
 def genArgParser():
@@ -43,6 +54,7 @@ def genArgParser():
     return args
 
 if __name__ == '__main__':
+    # python ComputeIncrease.py -start 2019_04_05 -end 2019_04_06
     input_args = genArgParser()
     start = input_args.start
     end = input_args.end
